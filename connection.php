a33975e4fc880676b8fe3b1c7055f050cde194d6
Snehal Khedekar <37830157+snehal1098@users.noreply.github.com>
2020-04-05 19:16:26 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -3,7 +3,6 @@
        	$username = "kesgfz2lwz2vdgo2";  
        	$password = "bb6obxfjmg0u8yaj";  
        	$dbname = "e8xfnsm9jnj53maa";
-	$port = "3306";
-       	$conn = mysqli_connect ($servername , $username , $password, $port) or die("unable to connect to host");  
+       	$conn = mysqli_connect ($servername , $username , $password) or die("unable to connect to host");  
 	$sql = mysqli_select_db ($conn,$dbname) or die("unable to connect to database"); 
 ?>
