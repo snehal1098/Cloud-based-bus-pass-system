039b42ef27ed7c56b5a383557e9456df900c9522
Snehal Khedekar <37830157+snehal1098@users.noreply.github.com>
2020-04-05 19:08:35 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php  
      	$servername = "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";  
-       	$username = "x2pq862ie6uczpum";  
-       	$password = "igpv2k3jdrl0kfcr";  
-       	$dbname = "t3b8ntuvzoix08a0";
+       	$username = "kesgfz2lwz2vdgo2";  
+       	$password = "bb6obxfjmg0u8yaj";  
+       	$dbname = "e8xfnsm9jnj53maa";
 	$port = "3306";
        	$conn = mysqli_connect ($servername , $username , $password, $port) or die("unable to connect to host");  
 	$sql = mysqli_select_db ($conn,$dbname) or die("unable to connect to database"); 
